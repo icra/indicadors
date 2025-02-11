3a7e860b680e88d830235b3ac7d5fe167b8579da
Lluís Bosch <holalluis@gmail.com>
2025-02-11 03:41:20 +0100

    update

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,15 @@
+#!/bin/bash
 
+#git
 git add *
 git commit -m "update"
 git push
+
+#desplega al servidor
+# -h  human readable format
+# -P  mostra progrés
+# -vv incrementa verbositat
+# -r  actua recursivament
+rsync -hPvr \
+  --exclude ".git" \
+  . root@icra.loading.net:/var/www/vhosts/icradev.cat/indicadors.icradev.cat
